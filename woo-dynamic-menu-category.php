92482b7e685005a432718798569a19ee1ef2a034
Numan Hussain <ahnuman5@gmail.com>
2020-02-26 13:17:51 +0600

    added plugin info

---
+++ woo-dynamic-menu-category.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Dynamic Menu Category
+ * Plugin URI: https://www.devute.com/wordpress/free-plugin/woo-dynamic-menu-category
+ * Description: Free WooCommerce automatic category menu. You can easily load your wooCommerce Categtory in your navigation menu or anywhere.
+ * Author: Numan
+ * Author URI: https://www.numanhussain.com
+ * Version: 1.0.0
+ * License: All Rights Reserved
+ *
+ */
