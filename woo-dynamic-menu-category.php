4ace2463ce82e1de9716f8b7ad5309a170c3faa8
Numan Hussain <ahnuman5@gmail.com>
2020-03-05 23:03:42 +0600

    fixed plugin info

--- woo-dynamic-menu-category.php
+++ woo-dynamic-menu-category.php
@@ -5,9 +5,12 @@
  * Description: Free WooCommerce automatic category menu. You can easily load your wooCommerce Categtory in your navigation menu or anywhere.
  * Author: Numan
  * Author URI: https://www.numanhussain.com
- * Version: 1.0
- * License: All Rights Reserved*
+ * Version: 1.0.0
+ * License: GPLv2 or Later
+ * Text Domain: wdmc
+ * Domain path: languages
  */
+
 
 if(!defined(ABSPATH)){
   die("Access denied");
